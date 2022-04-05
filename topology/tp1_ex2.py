7f5e5554cf239427766aa41b977a0ad2c27ee326
Eduardo Cunha - UM <92987154+pg45515@users.noreply.github.com>
2022-04-05 18:30:35 +0100

    Update of topology exercise 2

--- topology/tp1_ex2.py
+++ topology/tp1_ex2.py
@@ -1,50 +1,113 @@
+from mininet.net import Mininet
 from mininet.topo import Topo
+from mininet.node import RemoteController
+from mininet.node import OVSSwitch
+from mininet.cli import CLI
+from mininet.log import info
+from mininet.log import setLogLevel
 from mininet.link import TCLink
+from subprocess import call
 
-class MyTopo( Topo ):
-    "Topology Exercise 2"
+    #"Topology Exercise 2"
 
-    def build( self ):
-        "Create custom topo."
-        inf("*** CREATING NETWORK ***")
+def Topology():
+    #"Create custom topo."
 
-        net = Mininet(r)
+    info('Net A -> 192.168.1.0/24\nNet B -> 192.168.2.0/24\nNet C -> 192.168.3.0/24\n')
 
-        # Add hosts and switches
-        h1_A = self.addHost( 'h1_A', ip = '192.168.1.1/24' )   # Host 1 Net A
-        h2_A = self.addHost( 'h2_A', ip = '192.168.1.2/24' )   # Host 2 Net A
-        h3_A = self.addHost( 'h3_A', ip = '192.168.1.3/24' )   # Host 3 Net A
-        h1_B = self.addHost( 'h1_B', ip = '192.168.2.1/24' )   # Host 1 Net B
-        h2_B = self.addHost( 'h2_B', ip = '192.168.2.2/24'  )   # Host 2 Net B
-        h3_B = self.addHost( 'h3_B', ip = '192.168.2.3/24'  )   # Host 3 Net B
-        h1_C = self.addHost( 'h1_C', ip = '192.168.3.1/24'  )   # Host 1 Net C
-        h2_C = self.addHost( 'h2_C', ip = '192.168.3.2/24'  )   # Host 2 Net C
-        h3_C = self.addHost( 'h3_C', ip = '192.168.3.3/24'  )   # Host 3 Net C
-        s1 = self.addSwitch( 's1' )     # L2 Switch Net A (no ip)
-        s2 = self.addSwitch( 's2' )     # L2 Switch Net B (no ip)
-        s3 = self.addSwitch( 's3' )     # L2 Switch Net C (no ip)
-        r1 = self.addSwitch( 'r1' )     # L3 Switch
+    net = Mininet( controller=RemoteController, link=TCLink, switch=OVSSwitch )
+
+    info('***Creating remote controller on port 6633 (L2 switches)\n')
+    c0 = net.addController(name='c0',
+                        controller=RemoteController,
+                        ip='127.0.0.1',
+                        protocol='tcp',
+                        port=6633)
+    
+    info('***Creating remote controller on port 6655 (L3 switch)\n')
+    c1 = net.addController(name='c1',
+                        controller=RemoteController,
+                        ip='127.0.0.1',
+                        protocol='tcp',
+                        port=6655)
+
+    info('Adding L3 switch\n')
+    r1 = net.addSwitch('r1', cls = OVSSwitch, dpid = '0000000000000001')  # L3 switch
+
+    info('Adding L2 switches\n')
+    s1 = net.addSwitch('s1', cls=OVSSwitch, dpid='0000000000000002') # L2 Switch Net B (no ip)
+    s2 = net.addSwitch('s2', cls=OVSSwitch, dpid='0000000000000003') # L2 Switch Net C (no ip)
+    s3 = net.addSwitch('s3', cls=OVSSwitch, dpid='0000000000000004') # L2 Switch Net C (no ip)
+
         
 
+    info('Add hosts and switches\n')
+    h1_A = net.addHost( 'h1_A', ip = '192.168.1.1/24', mac = '00:00:00:00:00:01', defaultRoute='via 192.168.1.254')   # Host 1 Net A
+    h2_A = net.addHost( 'h2_A', ip = '192.168.1.2/24', mac = '00:00:00:00:00:02', defaultRoute='via 192.168.1.254' )   # Host 2 Net A
+    h3_A = net.addHost( 'h3_A', ip = '192.168.1.3/24', mac = '00:00:00:00:00:03', defaultRoute='via 192.168.1.254' )   # Host 3 Net A
+    h1_B = net.addHost( 'h1_B', ip = '192.168.2.1/24', mac = '00:00:00:00:00:04', defaultRoute='via 192.168.2.254' )   # Host 1 Net B
+    h2_B = net.addHost( 'h2_B', ip = '192.168.2.2/24', mac = '00:00:00:00:00:05', defaultRoute='via 192.168.2.254' )   # Host 2 Net B
+    h3_B = net.addHost( 'h3_B', ip = '192.168.2.3/24', mac = '00:00:00:00:00:06', defaultRoute='via 192.168.2.254' )   # Host 3 Net B
+    h1_C = net.addHost( 'h1_C', ip = '192.168.3.1/24', mac = '00:00:00:00:00:07', defaultRoute='via 192.168.3.254' )   # Host 1 Net C
+    h2_C = net.addHost( 'h2_C', ip = '192.168.3.2/24', mac = '00:00:00:00:00:08', defaultRoute='via 192.168.3.254' )   # Host 2 Net C
+    h3_C = net.addHost( 'h3_C', ip = '192.168.3.3/24', mac = '00:00:00:00:00:09', defaultRoute='via 192.168.3.254' )   # Host 3 Net C
+    
+    info('Adding links Net A\n')
+    net.addLink( h1_A, s1 )
+    net.addLink( h2_A, s1 )
+    net.addLink( h3_A, s1 )
+    net.addLink( r1, s1, cls=TCLink, delay = '5ms' )
 
-        # Add links
-        #Net A
-        self.addLink( h1_A, s1 )
-        self.addLink( h2_A, s1 )
-        self.addLink( h3_A, s1 )
-        self.addLink( s1, r1 )
+    info('\nAdding links Net B\n')
+    net.addLink( h1_B, s2 )
+    net.addLink( h2_B, s2 )
+    net.addLink( h3_B, s2, cls=TCLink, delay = '5ms' )
+    net.addLink( r1, s2 )
 
-        #Net B
-        self.addLink( h1_B, s2 )
-        self.addLink( h2_B, s2 )
-        self.addLink( h3_B, s2, cls=TCLink, delay = '5ms' )
-        self.addLink( s2, r1 )
+    info('\nAdding links Net C\n')
+    net.addLink( h1_C, s3 )
+    net.addLink( h2_C, s3 )
+    net.addLink( h3_C, s3, cls=TCLink, losses = '10')
+    net.addLink( r1, s3 )
 
-        #Net C
-        self.addLink( h1_C, s3 )
-        self.addLink( h2_C, s3 )
-        self.addLink( h3_C, s3, cls=TCLink, losses = 10)
-        self.addLink( s3, r1 )
+    net.build()
 
+    # Switches
+    r1.start([c1])
+    s1.start([c0])
+    s2.start([c0])
+    s3.start([c0])
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+    info('\nSetting up of IP addresses in the R1\n')
+    r1.cmd("ifconfig r1-eth1 0")
+    r1.cmd("ifconfig r1-eth2 0")
+    r1.cmd("ifconfig r1-eth3 0")
+    r1.cmd("ip addr add 192.168.1.254/24 brd + dev r1-eth1")
+    r1.cmd("ip addr add 192.168.2.254/24 brd + dev r1-eth2")
+    r1.cmd("ip addr add 192.168.3.254/24 brd + dev r1-eth3")
+    r1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+    info('Setting GW to hosts\n')
+    h1_A.cmd("ip route add default via 192.168.1.254")  # GW Net A
+    h2_A.cmd("ip route add default via 192.168.1.254")  # GW Net A
+    h3_A.cmd("ip route add default via 192.168.1.254")  # GW Net A
+    h1_B.cmd("ip route add default via 192.168.2.254")  # GW Net B
+    h2_B.cmd("ip route add default via 192.168.2.254")  # GW Net B
+    h3_B.cmd("ip route add default via 192.168.2.254")  # GW Net B
+    h1_C.cmd("ip route add default via 192.168.3.254")  # GW Net B
+    h2_C.cmd("ip route add default via 192.168.3.254")  # GW Net B
+    h3_C.cmd("ip route add default via 192.168.3.254")  # GW Net B
+
+    info('GW Net A -> 192.168.1.254/24\nGW Net B -> 192.168.2.254/24\nGW Net C -> 192.168.3.254/24\n')
+    #info('sudo mn --controller=remote,ip=127.0.0.1,port=6655\n')
+    #info('sudo mn --controller=remote,ip=127.0.0.1,port=6633\n')
+        
+    # Start command line
+    CLI(net)
+
+    # Stop Network
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    Topology()
