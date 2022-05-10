23f1fa4b64f0da8780fa47cbe287e00216587e95
pg41017 - UM <62184647+I-PingLin@users.noreply.github.com>
2022-05-10 21:17:33 +0100

    Update L3Switch.py

--- switches/L3Switch.py
+++ switches/L3Switch.py
@@ -7,7 +7,7 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import ipv4
-#
+
 
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
