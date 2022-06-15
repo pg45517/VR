b402cf309fa4d2bb86428df76a4feaa2d4486643
Miguel Peixoto - UM <92986801+pg45517@users.noreply.github.com>
2022-06-15 18:27:23 +0100

    code cleanup and file renaming

---
+++ topology/tp1_ex1_topology.py
@@ -0,0 +1,21 @@
+from mininet.topo import Topo
+
+class MyTopo(Topo):
+
+    def build( self ):
+        "Creating TP1 Exercise 1 topology..."
+
+        # Add hosts and switches
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        s1 = self.addSwitch('s1')
+
+        # Add links
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s1)
+
+topos = {'tp1_ex1_topo': (lambda: MyTopo())}
